44e2c3115de0f40d2d424fab58a9352c0130664a
Fangde Liu <hibana1983@gmail.com>
2018-07-14 13:08:50 +0800

    add setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+setup(name='tensor-layer-db',
+      version='0.1',
+      description='the model life cycle managment system',
+      url='https://github.com/fangde/TensorLab',
+      author='Fangde Liu',
+      author_email='liufangde@surgicalai.cn',
+      license='MIT',
+      packages=['tensorlab'],
+      install_requires=['pymongo'],
+      zip_safe=False)
